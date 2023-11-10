c51caccb71c127bc44507bad3784247dd4ba8b67
pseudocat <sydneyang1234@gmail.com>
2023-11-10 20:52:12 +0800

    update-html-code

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,1 +1,14 @@
-student homepage
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <article>
+        <h1>My Blog Post</h1>
+    </article>
+</body>
+</html>
