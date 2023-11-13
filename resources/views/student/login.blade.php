1a45ba707311f73ec8c748515afc608dadde26b8
pseudocat <sydneyang1234@gmail.com>
2023-11-13 23:32:01 +0800

    finished-welcome-page

--- resources/views/student/login.blade.php
+++ resources/views/student/login.blade.php
@@ -3,27 +3,25 @@
 <x-messages />
 <div class="min-h-screen min-w-full flex flex-col md:flex-row  ">
     <!--left-->
-    <div class="bg-white ouryellowbg shadow-lg rounded-lg md:w-1/2 sm:w-full min-h-full m-5 md:mr-0  ">
-        <div class="m-3 p-3 md:p-8 rounded-lg flex flex-col relative"style=" height:96%; ">
-            <div class="mb-6">
-                <h1 class="text-3xl font-bold text-white">Welcome OSAS Web Services</h1>
-                <p class="mt-4 text-white text-xl">To get started, please provide the following information to create
-                    your student account.</p>
+    <div class="bg-white rounded-lg md:w-1/2 sm:w-full min-h-full m-5 md:mr-0  ">
+        <div class="m-3 p-3 md:p-8 rounded-lg flex flex-col relative usep_background"style=" height:96%; ">
+            <div class="mb-6 flex flex-col justify-center items-center">
+                <h1 class="text-3xl font-bold text-white">Welcome to OSAS WEB SERVICES!</h1>
+                <p class="mt-4 text-white text-xl" style="text-align: justify;">Here we are, we are the dreamer. We'll make it happen, is you're a believer.</p>
             </div>
-            <div class="hidden md:block absolute inset-0 flex items-center justify-center">
+            <div class="hidden md:block inset-0 flex items-center justify-center">
                 <!-- Image at the very center -->
-                <img src="/images/student/login_image.png" alt="" class="mt-12 mx-auto w-80 ">
-                {{-- public\images\student\login_image.png --}}
+                <img src="/img/Ellips.png" alt="" class="mt-12 mx-auto w-80 ">
             </div>
-            <div class="flex justify-center mt-auto">
+                <div class="flex justify-center mt-auto">
                 <div class="w-12 h-12 rounded-full ouryellowbg flex items-center justify-center">
-                    <i class='bx bxl-facebook-square' style='color:#ffffff; font-size: 30px;'></i>
+                    <i class='bx bxl-facebook-square' style='color:#ffffff; font-size: 30px;'   ></i>
                 </div>
                 <div class="w-12 h-12 rounded-full ouryellowbg flex items-center justify-center ml-4">
                     <i class='bx bx-world' style="color:#ffffff; font-size: 30px;"></i>
                 </div>
                 <div class="w-12 h-12 rounded-full ouryellowbg flex items-center justify-center ml-4">
-                    <i class='bx bxl-gmail' style='color:#ffffff; font-size: 30px;'></i>
+                    <i class='bx bxl-gmail' style='color:#ffffff; font-size: 30px;' ></i>
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,27 @@
-{{-- @include('partials.__header') --}}
-
-<!-- testing github -->
-<!-- test 2 -->
-
-{{-- <div class="flex flex-col justify-center items-center ">
-    <img src="{{ asset('images/osaslogo.png') }}" class="h-10 mt-20 sm:h-40" />
-    <p class="text-dark text-5xl font-medium mt-4">OSAS</p>
-    <p class="text-dark text-3xl font-medium">Web Services</p>
-    <a href="{{ route('student_showlogin') }}">
-        <button type="button"
-            class="mt-20 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-            GET STARTED
-        </button>
-    </a>
-</div>
-
-@include('partials.__footer') --}}
-
 @include('partials.__header')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Inter, ui-sans-serif, system-ui, -apple-system, system-ui, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+        }
+    </style>
 
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+</head>
+<body>
     <x-messages />
-    <div class="min-h-screen min-w-full flex flex-col md:flex-row  ">
-        <!--left-->
-        {{-- <div class="bg-white rounded-lg md:w-1/2 sm:w-full min-h-full m-5 md:mr-0  ">
-            <div class="m-3 p-3 md:p-8 rounded-lg flex flex-col relative usep_background"style=" height:96%; ">
-                <div class="hidden md:block inset-0 flex flex-col">
-                    <!-- Image at the very center -->
-                    <img src="/images/student/get-started-img.png" alt="usep-students-talking">
-                </div>
-            </div>
-        </div> --}}
+    <div class="min-h-screen min-w-full flex flex-col md:flex-row">
         <div class="bg-white rounded-lg md:w-1/2 sm:w-full min-h-full m-5 md:mr-0  ">
             <div class="m-3 p-3 md:p-8 rounded-lg flex flex-col relative usep_background"style=" height:96%; ">
                 <div class="flex justify-center mt-auto">
@@ -40,28 +32,19 @@
         </div>
     <!--right-->
         <div class="p-8 bg-white rounded-lg min-h-full m-5 mt-0 md:w-1/2 min-h-full flex flex-col sm:w-full md:mt-5">
-            <form action="/admin/login/process" method="POST" class="h-full flex flex-col">
-                @csrf
-                <h1 class="text-xl font-bold text-gray-900">Admin Login</h1>
-                <div class="mt-4">
-                    <label for="email" class="block text-sm font-medium text-gray-900">Email</label>
-                    <input type="email" name="email" id="email" class="mt-2 px-2 py-1 w-full rounded-full border
-                    border-gray-300 focus:outline-none focus:border-yellow-400" value="">
-                </div>
-                <div class="mt-2">
-                    <label for="password" class="block text-sm font-medium text-gray-900">Password</label>
-                    <input type="password" name="password" id="Last Name" class="mt-2 px-2 py-1 w-full rounded-full border border-gray-300 focus:outline-none focus:border-yellow-400">
-
-                </div>
-                <a href="#" class="ml-2 mt-4 text-blue-500 text-sm  hover:text-blue-400">Forgot Password</a>
-
-                <div class="mt-6">
-                    <input type="submit" class="block w-full bg-red-800 hover:bg-red-900 text-white font-medium py-2 rounded-full text-center transition duration-300" value="Login">
-                </div>
-                <div class="lg:mt-auto mt-4 text-gray-700 text-base">
-                    <p>By signing up, you agree to our <span class="ouryellow font-bold">Terms of Service</span> and <span class="ouryellow font-bold">Privacy Policy</span>.</p>
-                </div>
-            </form>
+            <div class="flex flex-col h-auto justify-center items-center">
+                <img src="images/student/osas-logo.png" alt="OSAS Logo" style="height: 70%;">
+                <p style="font-weight: 800; letter-spacing: 5px; font-size: 48px;">OSAS WEB SERVICES</p>
+                <p style="font-weight: 700; letter-spacing: 3px;">OFFICE OF STUDENT AFFAIRS AND SERVICES</p>
+            </div>
+            <br>
+            <div class="mt-6 flex flex-col h-auto justify-center items-center">
+                <a href="{{ route('student_showlogin') }}">
+                    <button class="block w-auto px-14 py-2 rounded-full text-center transition duration-300" style="background-color: #fec24a; color: black; box-shadow: #252525; font-size: 20px; font-weight: 700; letter-spacing: 1px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.5)">GET STARTED</button>
+                </a>
+            </div>
         </div>
     </div>
+</body>
+</html> 
 @include('partials.__footer')
