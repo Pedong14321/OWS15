c0899348a012226b8858e7c14590d5d0340ca763
Pedong14321 <alfredjrvitangcor@gmail.com>
2023-12-04 21:57:51 +0800

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -45,6 +45,9 @@ Route::group(['prefix' => 'admin'], function () { // all routes here have /admin
         ->name('admin.grantees');
         Route::post('/admin/savesgrant', [AdminController::class,'savesgrant'])
         ->name('admin.savesgrant');
+        Route::post('/admin/EditGrants', [AdminController::class,'EditGrants'])
+        ->name('admin.EditGrants');
+        Route::get('/admin/EditGrant', [AdminController::class,'EditGrant'])->name('admin.EditGrant');
 
 
         //-------------------------for functionality routing-------------------------
